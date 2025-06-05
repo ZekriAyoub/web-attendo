60adc564dd68acd83c889a83427097f5f4a94722
Zekri <ayoub.zekri.3@icloud.com>
2025-06-05 12:24:10 +0200

    update supabaseClient

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = import.meta.env.VITE_SUPABASE_URL
-const key = import.meta.env.VITE_SUPABASE_KEY
-
-if (!url || !key) {
-  throw new Error("Missing Supabase environment variables. Check Vercel config.")
-}
-
-export const supabase = createClient(url, key)
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+)
